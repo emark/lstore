e699d56d14bcfdd87a297f52ef76b143a8340153
Sviridenko Maxim <emark@web2buy.ru>
2011-03-30 19:21:22 +0800

    create function on add recipes

--- lstore.pl
+++ lstore.pl
@@ -2,14 +2,23 @@
 use strict;
 use Mojolicious::Lite;
 use DBI;
+use utf8;
 
 our $dbh = DBI->connect("dbi:mysql:dbname=STORESTO;host=localhost",'root', 'admin',
 			{ PrintError => 0, RaiseError => 1 });
-
-# Documentation browser under "/perldoc" (this plugin requires Perl 5.10)
-plugin 'pod_renderer';
+$dbh->{'mysql_enable_utf8'} = 1;
+$dbh->do('SET NAMES utf8');
 
 get '/kitchen' => sub{
+  my $self=shift;
+  my $recipes=$dbh->selectall_hashref('SELECT RECIPE.ID,RECIPE.NAME,RECIPE.WEIGHT,SUM(ROUTER2.YIELD) AS YIELD FROM RECIPE LEFT JOIN ROUTER2 ON RECIPE.ID=ROUTER2.RECIPEID GROUP BY RECIPE.ID','ID');
+  $self->stash(teamail=>'team@storesto.ru',
+	       recipes=>$recipes,
+	       );  
+  $self->render(template=>'kitchen');
+};
+
+post '/kitchen' => sub{
   my $self=shift;
   my $recipeid=$self->param('recipeid') || undef;
   my $action=$self->param('action') || undef;
@@ -22,7 +31,7 @@ get '/kitchen' => sub{
     my $sth=$dbh->prepare($SQL{$action});
     $sth->execute;
   }
-  my $recipes=$dbh->selectall_hashref('SELECT RECIPE.ID,RECIPE.NAME,SUM(ROUTER2.YIELD) AS YIELD FROM RECIPE LEFT JOIN ROUTER2 ON RECIPE.ID=ROUTER2.RECIPEID GROUP BY RECIPE.ID','ID');
+  my $recipes=$dbh->selectall_hashref('SELECT RECIPE.ID,RECIPE.NAME,RECIPE.WEIGHT,SUM(ROUTER2.YIELD) AS YIELD FROM RECIPE LEFT JOIN ROUTER2 ON RECIPE.ID=ROUTER2.RECIPEID GROUP BY RECIPE.ID','ID');
   $self->stash(teamail=>'team@storesto.ru',
 	       recipes=>$recipes,
 	       );  
@@ -34,12 +43,6 @@ get '/' => sub {
   $self->render(status=>'503',text=>'Sorry, service is temporary unavailable.');
 };
 
-get '/hello' => sub {
-  my $self = shift;
-  $self->stash('teamail'=>'team@storesto.ru');
-  $self->render('welcome');
-};
-
 get '/recipe/:action/:id' => sub{
   my $self=shift;
   my $id=$self->stash('id');
@@ -48,7 +51,7 @@ get '/recipe/:action/:id' => sub{
   {
     my $recipename=$dbh->selectrow_array("SELECT NAME FROM RECIPE WHERE ID=$id");
     my $ingredients=$dbh->selectall_hashref("SELECT INGREDIENTS.ID,INGREDIENTS.NAME AS INGREDIENT,MEASURE.NAME AS MEASURE,RECIPE.NAME AS RECIPE, ROUTER1.VALUE FROM RECIPE INNER JOIN ROUTER1 ON RECIPE.ID=ROUTER1.RECIPEID LEFT JOIN INGREDIENTS ON  INGREDIENTS.ID=ROUTER1.INGRIDIENTID LEFT JOIN MEASURE ON INGREDIENTS.MEASUREID=MEASURE.ID WHERE ROUTER1.RECIPEID=$id",'ID');
-    my $instructions=$dbh->selectall_hashref("SELECT ROUTER3.ID,INGREDIENTS.NAME AS INGREDIENT,INSTRUCTIONS.STEP,INSTRUCTIONS.WAIT,INSTRUCTIONS.DESCRIBE,ROUTER3.VALUE FROM INSTRUCTIONS RIGHT JOIN ROUTER3 ON INSTRUCTIONS.ID=ROUTER3.INSTRUCTIONID LEFT JOIN INGREDIENTS ON ROUTER3.INGREDIENTID=INGREDIENTS.ID WHERE INSTRUCTIONS.RECIPEID=$id ORDER BY INSTRUCTIONS.STEP",'ID',,);
+    my $instructions=$dbh->selectall_hashref("SELECT ROUTER3.ID,INGREDIENTS.NAME AS INGREDIENT,INSTRUCTIONS.STEP,INSTRUCTIONS.WAIT,INSTRUCTIONS.DESCRIBE,ROUTER3.VALUE,MEASURE.NAME AS MEASURE FROM INSTRUCTIONS RIGHT JOIN ROUTER3 ON INSTRUCTIONS.ID=ROUTER3.INSTRUCTIONID LEFT JOIN INGREDIENTS ON ROUTER3.INGREDIENTID=INGREDIENTS.ID LEFT JOIN MEASURE ON INGREDIENTS.MEASUREID=MEASURE.ID WHERE INSTRUCTIONS.RECIPEID=$id ORDER BY INSTRUCTIONS.STEP",'ID',,);
     my $ascsort=$dbh->selectall_arrayref("SELECT ROUTER3.ID FROM INSTRUCTIONS RIGHT JOIN ROUTER3 ON INSTRUCTIONS.ID=ROUTER3.INSTRUCTIONID LEFT JOIN INGREDIENTS ON ROUTER3.INGREDIENTID=INGREDIENTS.ID WHERE INSTRUCTIONS.RECIPEID=$id ORDER BY INSTRUCTIONS.STEP");
     $self->stash(ingredients=>$ingredients,
 	       recipename=>$recipename,
@@ -56,28 +59,45 @@ get '/recipe/:action/:id' => sub{
 	       ascsort=>$ascsort,
 	       );
     $self->render('recipelist');
+  }elsif($action eq 'add'){
+    if($id eq '0'){
+      
+    }
+    my $ingredients=$dbh->selectall_hashref('SELECT INGREDIENTS.ID,INGREDIENTS.NAME,MEASURE.NAME AS MEASURE FROM INGREDIENTS LEFT JOIN MEASURE ON INGREDIENTS.MEASUREID=MEASURE.ID ORDER BY INGREDIENTS.NAME','ID');
+    $self->stash(ingredients=>$ingredients);
+    $self->render('addnewrecipe');
   }
 };
 
 get '/menu' => sub{
   my $self=shift;
+  my $recipes=$dbh->selectall_hashref('SELECT RECIPE.ID,RECIPE.NAME,ROUTER2.YIELD FROM RECIPE RIGHT JOIN ROUTER2 ON RECIPE.ID=ROUTER2.RECIPEID GROUP BY RECIPE.ID','ID');
+  $self->stash(recipes=>$recipes);
+  $self->render(template=>'menu');
+};
+
+post '/menu' => sub{
+  my $self=shift;
   my @id=$self->param('recipeid');
   my @yield=$self->param('yield');
-  my $action=$self->stash('action');
   my $sql;
   if(@id)
   {
     for(my $n=0;$n<@id;$n++)
     {
-      $sql="UPDATE ROUTER2 SET YIELD=$yield[$n] WHERE RECIPEID=$id[$n]";
+      if(!$yield[$n] || $yield[$n]==0)
+      {
+	$sql="DELETE FROM ROUTER2 WHERE RECIPEID=$id[$n]";
+      }else{
+	$sql="UPDATE ROUTER2 SET YIELD=$yield[$n] WHERE RECIPEID=$id[$n]";	
+      }
       my $sth=$dbh->prepare($sql);
       $self->stash(sql=>$sql,n=>$n);
       $sth->execute;
     }
   }
   my $recipes=$dbh->selectall_hashref('SELECT RECIPE.ID,RECIPE.NAME,ROUTER2.YIELD FROM RECIPE RIGHT JOIN ROUTER2 ON RECIPE.ID=ROUTER2.RECIPEID','ID');
-  $self->stash(recipes=>$recipes,
-	       );
+  $self->stash(recipes=>$recipes);
   $self->render(template=>'menu');
 };
 
@@ -89,7 +109,7 @@ get '/menu/:action' => sub{
     $self->stash(ingredients=>$ingredients);
     $self->render(template=>'viewmenu');
   }elsif($action eq 'prepare'){
-    my $instructions=$dbh->selectall_hashref("SELECT ROUTER3.ID,RECIPE.ID AS RECIPEID,RECIPE.NAME AS RECIPE,INSTRUCTIONS.STEP,ROUTER2.YIELD,INSTRUCTIONS.DESCRIBE,INGREDIENTS.NAME AS INGREDIENT,SUM(ROUTER3.VALUE*ROUTER2.YIELD) AS VALUE,INSTRUCTIONS.WAIT FROM ROUTER2 LEFT JOIN RECIPE ON ROUTER2.RECIPEID=RECIPE.ID INNER JOIN INSTRUCTIONS ON RECIPE.ID=INSTRUCTIONS.RECIPEID LEFT JOIN ROUTER3 ON INSTRUCTIONS.ID=ROUTER3.INSTRUCTIONID LEFT JOIN INGREDIENTS ON ROUTER3.INGREDIENTID=INGREDIENTS.ID GROUP BY RECIPE.ID, ROUTER3.INSTRUCTIONID,ROUTER3.INGREDIENTID",'ID');
+    my $instructions=$dbh->selectall_hashref("SELECT ROUTER3.ID,RECIPE.ID AS RECIPEID,RECIPE.NAME AS RECIPE,INSTRUCTIONS.STEP,ROUTER2.YIELD,INSTRUCTIONS.DESCRIBE,INGREDIENTS.NAME AS INGREDIENT,SUM(ROUTER3.VALUE*ROUTER2.YIELD) AS VALUE,INSTRUCTIONS.WAIT,MEASURE.NAME AS MEASURE FROM ROUTER2 LEFT JOIN RECIPE ON ROUTER2.RECIPEID=RECIPE.ID INNER JOIN INSTRUCTIONS ON RECIPE.ID=INSTRUCTIONS.RECIPEID LEFT JOIN ROUTER3 ON INSTRUCTIONS.ID=ROUTER3.INSTRUCTIONID LEFT JOIN INGREDIENTS ON ROUTER3.INGREDIENTID=INGREDIENTS.ID LEFt JOIN MEASURE On INGREDIENTS.MEASUREID=MEASURE.ID GROUP BY RECIPE.ID, ROUTER3.INSTRUCTIONID,ROUTER3.INGREDIENTID",'ID');
     my $ascsort=$dbh->selectall_arrayref("SELECT ROUTER3.ID FROM ROUTER2 LEFT JOIN RECIPE ON ROUTER2.RECIPEID=RECIPE.ID INNER JOIN INSTRUCTIONS ON RECIPE.ID=INSTRUCTIONS.RECIPEID LEFT JOIN ROUTER3 ON INSTRUCTIONS.ID=ROUTER3.INSTRUCTIONID LEFT JOIN INGREDIENTS ON ROUTER3.INGREDIENTID=INGREDIENTS.ID GROUP BY RECIPE.ID, ROUTER3.INSTRUCTIONID,ROUTER3.INGREDIENTID");
     $self->stash(instructions=>$instructions,
 		 ascsort=>$ascsort,
