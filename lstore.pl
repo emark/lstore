3d321d5dd4f99d7111e71fa2d732ebe51c60edcd
Sviridenko Maxim <emark@web2buy.ru>
2011-04-19 18:24:45 +0800

    create refrigirator

--- lstore.pl
+++ lstore.pl
@@ -19,9 +19,39 @@ get '/' => sub{
     $self->render(text=>'Hello storesto! Coming soon!');
 };
 
-get 'kitchen'=>sub{
+get '/kitchen'=>sub{
   my $self=shift;
   $self->render('kitchen');
+};
+
+get '/ingredients'=>sub{
+  my $self=shift;
+  my $ingredients=$dbh->selectall_hashref("SELECT INGREDIENTS.ID,INGREDIENTS.NAME AS INGREDIENT,MEASURE.NAME AS MEASURE FROM INGREDIENTS LEFT JOIN MEASURE ON INGREDIENTS.MEASUREID=MEASURE.ID",'ID');
+  $self->stash(ingredients=>$ingredients);
+  $self->render('ingredients');
+};
+
+get '/ingredients/:action/:id'=>sub{
+  my $self=shift;
+  my $action=$self->param('action');
+  my $id=$self->param('id');
+  if($action eq 'delete'){
+    $dbh->do("DELETE FROM INGREDIENTS WHERE ID=$id");
+  }
+  my $ingredients=$dbh->selectall_hashref("SELECT INGREDIENTS.ID,INGREDIENTS.NAME AS INGREDIENT,MEASURE.NAME AS MEASURE FROM INGREDIENTS LEFT JOIN MEASURE ON INGREDIENTS.MEASUREID=MEASURE.ID",'ID');
+  $self->stash(ingredients=>$ingredients);
+  $self->render(text=>'Ingredient deleted successfully');
+};
+post '/ingredients/'=>sub{
+  my $self=shift;
+  my $ingredient=$self->param('ingredient');
+  my $measure=$self->param('measure');
+  if($ingredient && $measure){
+    $dbh->do("INSERT INTO INGREDIENTS(ID,NAME,MEASUREID) VALUES(NULL,\"$ingredient\",$measure)");
+  }
+  my $ingredients=$dbh->selectall_hashref("SELECT INGREDIENTS.ID,INGREDIENTS.NAME AS INGREDIENT,MEASURE.NAME AS MEASURE FROM INGREDIENTS LEFT JOIN MEASURE ON INGREDIENTS.MEASUREID=MEASURE.ID",'ID');
+  $self->stash(ingredients=>$ingredients);
+  $self->render('ingrediens');
 };
 
 app->secret('storestosecret');
@@ -29,14 +59,28 @@ app->start;
 __DATA__
 
 @@ kitchen.html.ep
-% layout 'kitchenarea', title 'My kitchen';
+% layout 'default', title 'My kitchen';
 <%= link_to 'Create recipe'=>'recipe' %> / <%= link_to 'Open refrigirator'=>'ingredients' %>
 
-@@ layouts/kitchenarea.html.ep
+@@ ingredients.html.ep
+% layout 'default',title 'My refrigirator';
+%#	Create new ingredient
+<%= form_for 'ingredients'=>(method=>'post')=>begin %>
+<%= text_field 'ingredient' %>
+<%= select_field measure=>[['pcs'=>1],['g.'=>2],['ml.'=>3],['l.'=>4],['kg.'=>5]]%>
+<%= submit_button 'Add' %>
+<% end %>
+%#	Generate ingredients refs
+%foreach my $key(keys %{$ingredients}){
+  <%= $ingredients->{$key}{'INGREDIENT'}%>, <i><%= $ingredients->{$key}{'MEASURE'}%></i>, <%= link_to 'Delete'=>"ingredients/delete/$ingredients->{$key}{'ID'}" %><br/>
+%}
+
+@@ layouts/default.html.ep
 <title><%= title %></title>
-<body><%= content %></body>
-
-
+<body>
+<P><%= link_to 'Back to my kitchen'=>'kitchen'%></P>
+<%= content %>
+</body>
 
 
 
