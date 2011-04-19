2a1b54f58955ccd6599bd8a198247e7835fe9718
Sviridenko Maxim <emark@web2buy.ru>
2011-04-19 16:57:58 +0800

    new page

--- lstore.pl
+++ lstore.pl
@@ -6,9 +6,10 @@ use DBI;
 use utf8;
 
 open (DBCONF,"< db.conf") || die "Error open dbconfig file";
-my $dbconf=<DBCONF>;
+my @dbconf=<DBCONF>;
 close DBCONF;
-our $dbh = DBI->connect($dbconf,'locumtes_STORE86', '123456',
+chomp $dbconf[2];
+our $dbh = DBI->connect($dbconf[0],$dbconf[1],$dbconf[2],
 			{ PrintError => 0, RaiseError => 1 });
 $dbh->{'mysql_enable_utf8'} = 1;
 $dbh->do('SET NAMES utf8');
@@ -18,9 +19,22 @@ get '/' => sub{
     $self->render(text=>'Hello storesto!');
 };
 
+get 'kitchen'=>sub{
+  my $self=shift;
+  $self->render('kitchen');
+};
+
 app->secret('storestosecret');
 app->start;
+__DATA__
 
+@@ kitchen.html.ep
+% layout 'kitchenarea', title 'My kitchen';
+<%= link_to 'Create recipe'=>'recipe' %>
+
+@@ layouts/kitchenarea.html.ep
+<title><%= title %></title>
+<body><%= content %></body>
 
 
 
