c63aeb63c33b80422dd48e74f34fb1578bf0f13a
Sviridenko Maxim <sviridenko.m@web2buy.ru>
2011-07-18 19:40:16 +0800

    edit printform

---
+++ public/scripts.js
@@ -0,0 +1,13 @@
+function HSLayer()
+{
+		document.getElementById("message").style.display = "none";
+}
+function SelectLayer($div1,$div2)
+{
+		document.getElementById([$div1]).style.display = "none";
+		document.getElementById([$div2]).style.display = "";
+}
+function HideObject($object)
+{
+		document.getElementById([$object]).style.display = "none";
+}
